595dba4aa26aa51790a8ad61e5a961410c6b52b8
Gourav7727 <gauravverma43385@gmail.com>
2024-12-13 08:11:02 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+ = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
